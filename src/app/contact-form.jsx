89fa24e79c5d0b0a2c07689f30187790b18203b3
Axel Ledesma <axeldavidled999@gmail.com>
2025-11-21 17:40:15 -0300

    mejorando el responsive y problemas del textarea

--- src/app/contact-form.jsx
+++ src/app/contact-form.jsx
@@ -166,7 +166,6 @@ export function ContactForm() {
                     rows={4}
                     value={values.message}
                     className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                    defaultValue={""}
                   />
                   {touched.message && errors.message && (
                     <span style={{ color: "red" }}>{errors.message}</span>
